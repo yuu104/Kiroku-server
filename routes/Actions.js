cbdf94b5e5f6455e26e4762a99783b41e33da031
k20081kk <k20081kk@k20081kknoMacBook-Air.local>
2021-12-07 13:23:01 +0900

    refactor: APIリソース設計の変更

--- routes/Actions.js
+++ routes/Actions.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../connection");
 const {validateToken} = require("../middlewares/AuthMiddleware");
 
+// 登録したアクション一覧を取得
 router.get("/", validateToken, (req, res) => {
   const userId = req.user.id;
   const sqlSelect = `SELECT * FROM ActionItem WHERE user_id='${userId}'`
@@ -14,7 +15,8 @@ router.get("/", validateToken, (req, res) => {
   });
 });
 
-router.get("/edit/:id", (req, res) => {
+// 指定したアクションを取得
+router.get("/:id", (req, res) => {
   const id = req.params.id;
   const sqlSelect = `SELECT * FROM ActionItem WHERE id=${id}`
   db.getConnection((err, connection) => {
@@ -25,7 +27,8 @@ router.get("/edit/:id", (req, res) => {
   });
 });
 
-router.post("/add", validateToken , (req, res) => {
+// 新規アクション追加
+router.post("/", validateToken , (req, res) => {
   const name = req.body.item_name;
   const color = req.body.color;
   const userId = req.user.id;
@@ -38,7 +41,8 @@ router.post("/add", validateToken , (req, res) => {
   });
 });
 
-router.post("/update", (req, res) => {
+// アクションを編集
+router.put("/", (req, res) => {
   const id = req.body.id;
   const name = req.body.item_name;
   const color = req.body.color;
@@ -51,7 +55,8 @@ router.post("/update", (req, res) => {
   });
 });
 
-router.delete("/delete", (req, res) => {
+// アクションを削除
+router.delete("/", (req, res) => {
   const id = req.body.id;
   const sqlDelete = "DELETE FROM ActionItem WHERE id=?";
   db.getConnection((err, connection) => {
--- routes/Logs.js
+++ routes/Logs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../connection");
 const {validateToken} = require("../middlewares/AuthMiddleware");
 
+// 現在記録しているアクションがあるかどうかを調べるためのAPI
 router.get("/", validateToken, (req, res) => {
   const sqlSelect = "SELECT * FROM Time WHERE user_id=? AND finish_time IS NULL";
   db.getConnection((err, connection) => {
@@ -13,7 +14,8 @@ router.get("/", validateToken, (req, res) => {
   });
 });
 
-router.get("/timeChart/:dateString", validateToken, (req, res) => {
+// 指定日の行動記録データを取得（開始日と終了日が異なるデータも取得）
+router.get("/:dateString", validateToken, (req, res) => {
   const sqlSelect = `SELECT * FROM Time WHERE user_id=? AND finish_time IS NOT NULL AND (start_time LIKE '${req.params.dateString}%' OR finish_time LIKE '${req.params.dateString}%')`;
   db.getConnection((err, connection) => {
     connection.query(sqlSelect, [req.user.id], (err, result) => {
@@ -23,22 +25,24 @@ router.get("/timeChart/:dateString", validateToken, (req, res) => {
   });
 });
 
-router.post("/start", validateToken, (req, res) => {
-  const itemName = req.body.item_name;
-  const color = req.body.color;
-  const startTime = req.body.start_time;
-  const userId = req.user.id;
-  const sqlInsert = "INSERT INTO Time (user_id, item_name, color , start_time) VALUES (?, ?, ?, ?)";
-  db.getConnection((err, connection) => {
-    connection.query(sqlInsert, [userId, itemName, color, startTime], (err, result) => {
-      res.json(result);
-      connection.release();
-    });
-  });
-});
+// // 記録をスタート
+// router.post("/start", validateToken, (req, res) => {
+//   const itemName = req.body.item_name;
+//   const color = req.body.color;
+//   const startTime = req.body.start_time;
+//   const userId = req.user.id;
+//   const sqlInsert = "INSERT INTO Time (user_id, item_name, color , start_time) VALUES (?, ?, ?, ?)";
+//   db.getConnection((err, connection) => {
+//     connection.query(sqlInsert, [userId, itemName, color, startTime], (err, result) => {
+//       res.json(result);
+//       connection.release();
+//     });
+//   });
+// });
 
-router.post("/stop", validateToken, (req, res) => {
-  const id = req.body.id;
+// 記録を終了して、終了時間を格納する
+router.patch("/:id/stop", validateToken, (req, res) => {
+  const id = req.params.id;
   const finishTime = req.body.finish_time;
   const sqlUpdate = "UPDATE Time SET finish_time=? WHERE id=?";
   db.getConnection((err, connection) => {
@@ -49,7 +53,8 @@ router.post("/stop", validateToken, (req, res) => {
   });
 });
 
-router.post("/add", validateToken, (req, res) => {
+// 記録を追加
+router.post("/", validateToken, (req, res) => {
   const userId = req.user.id;
   const itemName = req.body.item_name;
   const color = req.body.color;
@@ -64,8 +69,9 @@ router.post("/add", validateToken, (req, res) => {
   });
 });
 
-router.put("/edit", (req, res) => {
-  const id = req.body.id;
+// 記録の開始時間と終了時間を変更する
+router.patch("/:id/edit", (req, res) => {
+  const id = req.params.id;
   const startTime = req.body.start_time;
   const finishTime = req.body.finish_time;
   const sqlUpdate = "UPDATE Time SET start_time=?, finish_time=? WHERE id=?";
@@ -77,7 +83,8 @@ router.put("/edit", (req, res) => {
   });
 });
 
-router.delete("/delete", (req, res) => {
+// 記録を削除する
+router.delete("/", (req, res) => {
   const id = req.body.id;
   const sqlDelete = "DELETE FROM Time WHERE id=?";
   db.getConnection((err, connection) => {
